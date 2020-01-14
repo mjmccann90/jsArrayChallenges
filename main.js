957d4be93e5755e0fd41480b888a8594cf8725cd
Mark McCann <mjmccann12@me.com>
2020-01-14 09:26:23 -0600

    first commit with readme

---
+++ main.js
@@ -0,0 +1,6 @@
+/*
+Challenge #1: Highest Scoring Word
+
+Given a string of 2 words, find the highest scoring word. Each letter of a word scores points according to it's position in the alphabet: a = 1, b = 2, c = 3 etc. All letters will be lowercase there will be no punctuation. Display (id='challenge-1') the word that is the highest scoring. If two words score the same, display the word that appears earliest in the original string.
+*/
+
